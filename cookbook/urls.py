bd5acda0e036d097917a09301aade31628c0e152
Ubuntu <tayj0016@monticello.xd12lupkgkhefko0rtahrjeyzg.bx.internal.cloudapp.net>
2022-06-09 12:32:14 +0000

    update urls.py

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # path('', include('recipes.urls')),
-    # path('admin/', admin.site.urls),
-    # path('accounts/', include('django.contrib.auth.urls')),
-    # path('accounts/login', include('django.contrib.auth.urls')),
+    path('', include('recipes.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login', include('django.contrib.auth.urls')),
 ]
